8f6538777df3ebbb097ff806683b0f8a407fb5d9
ayoadeyera <guitarsolomon@gmail.com>
2026-09-24 12:58:03 +0100

    Implement categories feature: add getAllCategories function and update categories view

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import path from 'path';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
 import { getAllProjects } from './src/models/projects.js';
+import { getAllCategories } from './src/models/categories.js';
+
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -49,9 +51,12 @@ app.get('/projects', async (req, res) => {
     const title = 'Projects';
     res.render('projects', { title, projects });
 });
+
 app.get('/categories', async (req, res) => {
+    const categories = await getAllCategories();
+
     const title = 'Categories';
-    res.render('categories', { title });
+    res.render('categories', { title, categories });
 });
 
 const server = app.listen(PORT, async () => {
